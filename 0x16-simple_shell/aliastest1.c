51885f984839815b2126c83e66d580866104e671
pascallino <luvpascal.ojukwu@yahoo.com>
2023-05-12 15:03:49 +0100

    gg

--- 0x16-simple_shell/aliastest1.c
+++ 0x16-simple_shell/aliastest1.c
@@ -5,6 +5,9 @@
 #define MAX_ALIAS_NAME 50
 #define MAX_ALIAS_VALUE 100
 #define ALIAS_FILE "aliases.txt"
+#define MAX_COMMAND_LENGTH 1000
+#define MAX_NUM_ARGS 100
+
 
 // Function to load aliases from file
 void load_aliases() {
@@ -23,14 +26,21 @@ void load_aliases() {
 }
 
 void update_alias(const char* name, const char* value) {
-    FILE* file = fopen(ALIAS_FILE, "r+");
+    FILE* file = fopen(ALIAS_FILE, "r");
     if (file == NULL) {
         perror("Error opening aliases file");
         return;
     }
 
+    // Create a temporary file to write the updated aliases
+    FILE* temp = fopen("temp.txt", "w");
+    if (temp == NULL) {
+        perror("Error creating temporary file");
+        fclose(file);
+        return;
+    }
+
     char line[MAX_ALIAS_NAME + MAX_ALIAS_VALUE + 12];  // +12 for "alias name='value'\n"
-    char updated_line[MAX_ALIAS_NAME + MAX_ALIAS_VALUE + 12];
     int updated = 0; // Flag to indicate if the alias has been updated
 
     while (fgets(line, sizeof(line), file) != NULL) {
@@ -39,19 +49,31 @@ void update_alias(const char* name, const char* value) {
         if (sscanf(line, "alias %[^=]=\'%[^\']\'", alias_name, alias_value) == 2) {
             if (strcmp(alias_name, name) == 0) {
                 // Alias with the same name found, update the value
-                snprintf(updated_line, sizeof(updated_line), "alias %s='%s'\n", name, value);
-                fputs(updated_line, file);
+                fprintf(temp, "alias %s='%s'\n", name, value);
                 updated = 1;
+            } else {
+                // Write the existing alias to the temporary file
+                fprintf(temp, "%s", line);
             }
+        } else {
+            // Write any non-alias lines to the temporary file
+            fprintf(temp, "%s", line);
         }
     }
 
     // If the alias was not found, add it as a new entry
     if (!updated) {
-        fprintf(file, "alias %s='%s'\n", name, value);
+        fprintf(temp, "alias %s='%s'\n", name, value);
     }
 
     fclose(file);
+    fclose(temp);
+
+    // Rename the temporary file to the original file
+    if (rename("temp.txt", ALIAS_FILE) != 0) {
+        perror("Error renaming temporary file");
+        return;
+    }
 }
 
 
@@ -117,7 +139,21 @@ void process_command(const char* command) {
     }
 }
 
-
+int convertStringToArray(char* inputString, char** commandArray) {
+    // Trim leading and trailing spaces
+    char* trimmedString = strtok(inputString, " \t\n");
+    
+    // Split the string into individual elements
+    char* token = strtok(trimmedString, " \t\n");
+    int count = 0;
+    while (token != NULL) {
+        commandArray[count] = strdup(token);
+        count++;
+        token = strtok(NULL, " \t\n");
+    }
+    
+    return count;
+}
 
 int main() {
     char command[MAX_ALIAS_NAME + MAX_ALIAS_VALUE + 12];  // +12 for "alias name='value'\n"
@@ -127,22 +163,28 @@ int main() {
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = '\0';  // Remove trailing newline character
 
-        int i = 0;
+        int n, i = 0;
         int equals = 0;
+	char** commandArray = malloc(sizeof(char*) * strlen(command));
+	
         while (command[i]) {
             if (command[i] == '=') {
-                equals = 1;
-                break;
+                equals = equals +  1;
             }
             i++;
         }
+//handle multi registrstion of alias
+if (equals >= 2)
+{
+}
+else
+{
 
         if (strcmp(command, "alias") == 0) {
             load_aliases();
         } else if (strncmp(command, "alias ", 6) == 0 && strlen(command) > 6 && equals == 0) {
             char name[MAX_ALIAS_NAME];
-            char name2[MAX_ALIAS_NAME];
-            if (sscanf(command, "alias %s %s", name, name2) >= 1) {
+            if (sscanf(command, "alias %s", name) == 1) {
                 process_command(command);
             } else {
                 printf("Invalid command format. Usage: alias name\n");
@@ -160,6 +202,8 @@ int main() {
         } else {
             printf("Unknown command: %s\n", command);
         }
+}
+free(commandArray);
     }
 
     return 0;
